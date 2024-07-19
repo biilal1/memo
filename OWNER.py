e451bcb8a790925ec7ab5165f0fdf2f5c068d44a
01544488662 <am133737646@gmail.com>
2024-07-19 19:47:07 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -6,14 +6,14 @@ from pyrogram import filters
 
 load_dotenv()
 
-OWNER = ["TR_E2S_ON_MY_MOoN"]
-OWNER_NAME = " ? ? ? ? ? || ?? ? ??????? ? ? "
-BOT_TOKEN = getenv("BOT_TOKEN")
-DATABASE = getenv("MONGO_DB_URI", None)
+OWNER = ["M_9_T"]
+OWNER_NAME = "𓏺 𝘾َِ𝘳- 𝙉َِ𝘰َِ𝙐َِ𝘳 Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
+BOT_TOKEN = getenv("7327600929:AAGdB4xoKUeG8ReCUI2HHjvi6-RDuMj_jRQ")
+DATABASE = getenv("mongodb+srv://am133737646:<password>@cluster0.9h2dqs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 CHANNEL = "https://t.me/sourceav"
 GROUP = "https://t.me/va_source"
 VID_SO = "https://telegra.ph/file/4fda78aaf200bf313be62.jpg"
 PHOTO = "https://telegra.ph/file/4fda78aaf200bf313be62.jpg"
-LOGS = "jabababbab"
+LOGS = "M_9_T"
