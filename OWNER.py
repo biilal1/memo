abd441a5787606973f40fece8c038b5cf7373dd0
01544488662 <am133737646@gmail.com>
2024-07-19 20:35:57 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -9,7 +9,7 @@ load_dotenv()
 OWNER = ["M_9_T"]
 OWNER_NAME = "𓏺 𝘾َِ𝘳- 𝙉َِ𝘰َِ𝙐َِ𝘳 Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
 BOT_TOKEN = getenv("7327600929:AAHAwz7HCSfWnIbrzv_reoglpbxOAQeCAkY")
-DATABASE = getenv("mongodb+srv://am133737646:<password>@cluster0.9h2dqs6.mongodb.net/", None)
+DATABASE = getenv("mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 CHANNEL = "https://t.me/sourceav"
